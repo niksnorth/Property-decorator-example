4ac834ccd94bf9e5315551b38cb0d7df1e9f809a
niksnorth <40292597+niksnorth@users.noreply.github.com>
2019-01-29 17:14:11 +0000

    Add files via upload

---
+++ propertyExample.py
@@ -0,0 +1,25 @@
+class Celsius:
+    def __init__(self, temperature = 0):
+        self._temperature = temperature
+
+    def to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32
+
+    @property
+    def temperature(self):
+        print("Getting value")
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        if value < -273:
+            raise ValueError("Temperature below -273 is not possible")
+        print("Setting value")
+        self._temperature = value
+
+if __name__ == "__main__":
+    c = Celsius()
+    c.temperature = 137
+    print(c.temperature)
+    d = c.to_fahrenheit()
+    print(d)
